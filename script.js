3226e0285c11077889d62d4e8ab89cad0282b336
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-11-21 16:07:53 -0500

    feat: begin IIFE for game logic controller

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // IIFE Function gameboard (module pattern)
     // Pass in: n/a
     // Create a single instance of an object to represent the gameboard, whose properties are:
-        // the grid itself, a 2D array with 3 rows and 3 columns
+        // ✓ the grid itself, a 2D array with 3 rows and 3 columns
         // a function that reads (gets) the values of the entire gameboard (likely use a nested loop over the rows and cols)
-        // a function that reads (gets) the value of the specific space passed to it (parameters are the row and col indices)
-        // a function that modifies (sets) the value of the space passed to it with a given value (parameters are the row and col indicies and the given value)
+        // ✓ a function that reads (gets) the value of the specific space passed to it (parameters are the row and col indices)
+        // ✓ a function that modifies (sets) the value of the space passed to it with a given value (parameters are the row and col indicies and the given value)
     // Pass out: gameboard object
 // Endfunction
 
@@ -31,9 +31,9 @@ const gameBoard = (function() {
     // Pass out: player obj
 // Endfunction
 
-function createPlayer(name, mark) {
-    const playerName = name;
-    const playerMark = mark;
+function createPlayer(selectedName, selectedMark) {
+    const playerName = selectedName;
+    const playerMark = selectedMark;
 
     const setPlayerMark = (rowIndex, colIndex) => gameBoard.setGridSpaceVal(rowIndex, colIndex, playerMark);
 
@@ -45,9 +45,9 @@ function createPlayer(name, mark) {
 
 // IIFE Function game logic controller (module pattern)
     // Pass in: n/a
-    // Store the player instances (accessible via closure)
+    // ✓ Store the player instances (accessible via closure)
     // Create an object that keeps track of the game flow and logic, whose properties are:
-        // a function to record when a player makes a move (i.e. when a player obj instance calls a method to change a grid space), where the player made it (get the gameboard obj space indices from the move), and which player made the move (get player obj instance's name and value)
+        // ✓ a function to record when a player makes a move (i.e. when a player obj instance calls a method to change a grid space), where the player made it (get the gameboard obj space indices from the move), and which player made the move (get player obj instance's name and value)
         // a function to handle turn changes aka designate which player is currently active and which is not
         // a function to determine winning conditions (likely returns a boolean value)
         // a function to determine tying conditions i.e. gameboard spaces are full but no winning conditions are true
@@ -56,3 +56,13 @@ function createPlayer(name, mark) {
         // a function to begin a new game round (should set the gameboard spaces back to empty rows and cols, delete the player objs?)
     // Pass out: n/a
 // Endfunction
+
+const gameLogicController = (function() {
+    const playerOne = createPlayer();
+    const playerTwo = createPlayer();
+
+    // need to figure out how to designate the current player whose turn it is to make a move, we can then pass this returned value to makeCurrentPlayerMove
+
+    const makeCurrentPlayerMove = (currentPlayer) => currentPlayer.setPlayerMark();
+
+})()
