5f6fceb4d090b82a8958f09f0736a88c650054f3
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-01-15 14:25:02 -0500

    feat: update function that adds DOM elements representing gameboard spaces to display their respective gameboard content

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gameBoard = (function() {
     const grid = [
-        [[], [], []],
-        [[], [], []], 
-        [[], [], []]
+        [["X"], ["O"], ["O"]],
+        [["O"], ["X"], ["X"]], 
+        [["O"], [], ["O"]]
     ];
     const getGridSpaceVal = (rowIndex, colIndex) => grid[rowIndex][colIndex];
     const setGridSpaceVal = (rowIndex, colIndex, gridSpaceVal) => grid[rowIndex][colIndex] = gridSpaceVal;
@@ -141,21 +141,19 @@ const gameLogicController = (function() {
 
 // *** WE ARE HERE!!! ***
 // pseudocode!!
-// create an IIFE that returns an object with the necessary DOM elements + a function that renders the gameboard grid to the webpage 
-// loop over the grid array and create 9 child divs, which will act as board squares representing the 2D array values (each has a row and column attribute).
-// Next, create a rendering function to loop over the grid array again, this time set the textContent of each corresponding div of the HTML parent's children (use the children DOM property and find the right div using its row and column attributes) to the nested array's value (if empty, then just set it to an empty string)
-// We separate these two actions because we want the render function to be reusable (e.g. when a player makes a mark on the board, we don't want to recreate the whole board from scratch, just modify the right square and render it on the webpage)
-// Have the IIFE return an object with the HTML gameboard divs and the rendering function
 
 const gameDisplayController = (function() {
     const gameBoardGridArea = document.querySelector("#gameboard-grid-area");
 
-    const createGameBoard = () => {
+    const createAndRenderGameBoard = () => {
         for (let outerLoopIndex = 0; outerLoopIndex < gameBoard.grid.length; outerLoopIndex++) {    
             for (let innerLoopIndex = 0; innerLoopIndex < gameBoard.grid[outerLoopIndex].length; innerLoopIndex++) {
                 const gridSpaceSquare = document.createElement("div");
+
                 gridSpaceSquare.setAttribute("row", outerLoopIndex);
                 gridSpaceSquare.setAttribute("column", innerLoopIndex);
+
+                gridSpaceSquare.textContent = gameBoard.grid[outerLoopIndex][innerLoopIndex].length === 0 ? "" : gameBoard.grid[outerLoopIndex][innerLoopIndex];
                 gameBoardGridArea.append(gridSpaceSquare);
 
                 // this is just to see the divs, delete later
@@ -166,10 +164,10 @@ const gameDisplayController = (function() {
         }
     }
 
-    return { createGameBoard }
+    return { createAndRenderGameBoard }
 })()
 
-// gameDisplayController.createGameBoard();
+// gameDisplayController.createAndRenderGameBoard();
 
 // Testing/Debugging Process: play a game round!
 
